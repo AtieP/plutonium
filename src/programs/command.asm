236477fbd4cb21079b8fb85fec1c2a7b73bf7510
AtieP <nosequiensoy8664@gmail.com>
2020-04-18 11:39:54 +0200

    bee da be doo

--- src/programs/command.asm
+++ src/programs/command.asm
@@ -38,13 +38,40 @@ putchar:
     ret
 
 ; Gets a key from the keyboard and print it.
-; IN: Nothing
+; IN: CF set = Echo
 ; OUT: Character in AL, keycode in AH
 getchar:
     
     xor ax, ax
     int 16h
 
-    call putchar
-    
+    jc .echo
     ret
+
+.echo:
+
+    call putchar
+
+    ret
+
+; Gets a string from the keyboard.
+; IN: CX = Lenght, DI = String location
+; OUT: Nothing
+gets:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov dx, cx          ; initial possition
+
+.get_chars:
+
+    call get_chars
+
+    cmp ah, 0x1C        ; key enter
+    je .done
+
+    cmp ah, 0x0E        ; key backspace
+    je .handle_backspace
+
